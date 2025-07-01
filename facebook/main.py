2a5645b96e942bf2664613de8742d7fb42387a98
marytraore02 <marytra22@gmail.com>
2025-07-01 17:04:01 +0000

    facebook end

--- facebook/main.py
+++ facebook/main.py
@@ -17,11 +17,12 @@ FB_APP_ID = os.environ['FB_APP_ID']
 FB_APP_SECRET = os.environ['FB_APP_SECRET']
 APP_SECRET_KEY = os.environ['APP_SECRET_KEY']
 
-API_VERSION = "v19.0"
+API_VERSION = "v23.0"
 # IMPORTANT : Cette URI de redirection doit être ajoutée dans les paramètres de votre app Facebook
 # Section "Connexion Facebook" -> "Paramètres" -> "URI de redirection OAuth valides"
 # Exemple : http://127.0.0.1:8000/auth/facebook/callback
-REDIRECT_URI = "https://seneinnov.com/test.html"
+# REDIRECT_URI = "https://seneinnov.com/test.html"
+REDIRECT_URI = "https://dev.mon-app.com/auth/facebook/callback"
 
 # Initialisation de FastAPI
 app = FastAPI()
@@ -30,7 +31,6 @@ templates = Jinja2Templates(directory="templates")
 
 
 # --- Routes d'Authentification OAuth2 ---
-
 @app.get("/login/facebook")
 def login_facebook():
     """Redirige l'utilisateur vers Facebook pour l'authentification."""
@@ -113,6 +113,16 @@ def read_root(request: Request):
         "publish_result": publish_result
     })
 
+
+@app.get("/terms", response_class=HTMLResponse)
+def show_terms(request: Request):
+    """Affiche la page des conditions d'utilisation."""
+    return templates.TemplateResponse("terms.html", {"request": request})
+
+@app.get("/policy", response_class=HTMLResponse)
+def show_policy(request: Request):
+    """Affiche la page de politique de confidentialité."""
+    return templates.TemplateResponse("policy.html", {"request": request})
 
 @app.post("/publish")
 def publish_to_page(
